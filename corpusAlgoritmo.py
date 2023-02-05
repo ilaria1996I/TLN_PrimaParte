e31cb6eb9e5f9adf85acb9b310ad9bd9bf2543b5
ilaria1996I <59748882+ilaria1996I@users.noreply.github.com>
2023-02-05 14:21:01 +0100

    Update corpusAlgoritmo.py

--- corpusAlgoritmo.py
+++ corpusAlgoritmo.py
@@ -2,95 +2,97 @@ import operator
 import math
 
 #algoritmo di viterbi
-def viterbi(frase:list, trans_prob:dict, emission_prob:dict, smoothed:dict ):
+def algoritmoDiViterbi(frase:list, trans_prob:dict, emission_prob:dict, parole_sconosciute:dict ):
+    tag='tag_null' #verrà cambiato con quello corretto successivamente
+    numParoleDellaFrase = len(frase)
     vit = dict()
     path = dict()
     posPath = dict()
-    maxTag='_'
+    
+    
+    #1. setto la prima colonna
     for state in trans_prob.keys():
-        if(state != 'FIRST'):
+        if(state != 'FIRST'): #perche mi serve la seconda parola dove il tag precedente è FIRST
             vit[state] = list()
             path[state] = list()
-            if  frase[0] in smoothed:
-                if state in smoothed[frase[0]] and state in trans_prob['FIRST']:
-                    vit[state].append(  trans_prob['FIRST'][state] +  smoothed[frase[0]][state])
-                else:
-                    vit[state].append(-9999)
+            if  frase[0] in parole_sconosciute:
+                trans_probAndParole_sconosciute = -100000 #nel caso in cui non entra nel if sotto do un valore "nullo"
+                if state in parole_sconosciute[frase[0]] and state in trans_prob['FIRST']:
+                    trans_probAndParole_sconosciute = trans_prob['FIRST'][state] + parole_sconosciute[frase[0]][state]
+                vit[state].append(trans_probAndParole_sconosciute)
+                
             else:
+                trans_probAndEmission_prob = -100000 #nel caso in cui non entra nel if sotto do un valore "nullo"
                 if state in emission_prob[frase[0]] and state in trans_prob['FIRST']:
-                    vit[state].append(  trans_prob['FIRST'][state] +  emission_prob[frase[0]][state])
-                else:
-                    vit[state].append(-9999)
-            path[state].append(maxTag)
-    for i in range(len(frase)):
+                    trans_probAndEmission_prob = trans_prob['FIRST'][state] +  emission_prob[frase[0]][state]
+                vit[state].append(trans_probAndEmission_prob)
+            
+            path[state].append(tag)
+    
+    #2. setto le colonne successive alla prima
+    for i in range(numParoleDellaFrase):
         if(i>0):
-            mVit = -9999
+            mVit = -100000
             for state in trans_prob.keys():
                 if(state != 'FIRST'):
-                    mVit, mPath,  maxTag = maximum(vit, frase[i], i-1, maxTag,state, trans_prob, emission_prob, smoothed)
+                    mVit, mPath, tag = scegliValoreDiViterbiMiglioreFinoAlloStatoCorrente(vit, frase[i], i-1, tag,state, trans_prob, emission_prob, parole_sconosciute)
                     path[state].append(round(mPath,2))
                     vit[state].append(round(mVit,2))
-            posPath[frase[i-1]] = maxTag
+            posPath[frase[i-1]] = tag
 
-    mPath = -9999
-    temp = 0
+    mPath = -100000
+    ViterbiCambiato = 0
     lastTag = ''
-    for s in trans_prob.keys():
-        if s != 'FIRST':
-            if( 'LAST' in trans_prob[s]):
-                temp = vit[s][i]+ trans_prob[s]['LAST']
-                if temp > mPath:
-                    mPath = temp
-                    lastTag = s 
-    vit['END'] = mPath
+    for tagPreced in trans_prob.keys():
+        if tagPreced != 'FIRST':
+            if( 'LAST' in trans_prob[tagPreced]): #se siamo qui è l'ultima parola
+                ViterbiCambiato = vit[tagPreced][i]+ trans_prob[tagPreced]['LAST']
+                if ViterbiCambiato > mPath:
+                    mPath = ViterbiCambiato
+                    lastTag = tagPreced 
+    vit['LAST'] = mPath
     posPath[frase[i]] = lastTag
 
     return posPath, vit
 
-#algoritmo di Baseline
+#algoritmo di Baseline 
 def baseline(frase, tag_della_parola):
-    res = dict()
-    
+    dictForBaseLine = dict()
     for parola in frase:
-        if parola in tag_della_parola:
-            res[parola] = max(tag_della_parola[parola].items(), key=operator.itemgetter(1))[0]
-            
-        else:
-            res[parola] = 'MISC'
-    return res
+        if parola in tag_della_parola:dictForBaseLine[parola] = max(tag_della_parola[parola].items(), key=operator.itemgetter(1))[0]  
+        else:dictForBaseLine[parola] = 'MISC'
+    return dictForBaseLine
 
 #La funzione che preleva il massimo (viene utilizzata nell'algoritmo di viterbi)
-def maximum(vit, parola, i, maxTag, state,trans_prob, emission_prob, smoothed):
-    mVit = -9999
-    mPath = -9999
-    temp1 = 0
-    temp2 = 0
+def scegliValoreDiViterbiMiglioreFinoAlloStatoCorrente(vit, parola, i, tag, state,trans_prob, emission_prob, parole_sconosciute):
+    mVit = -100000
+    mPath = -100000
+    valoreViterbiCambiato = 0
+    pathViterbiCambiato = 0
 
-    for s in trans_prob.keys():
-        if s != 'FIRST':
-            if parola in smoothed:
-                if state in smoothed[parola] and state in trans_prob[s]:
-                    temp1 = vit[s][i] + trans_prob[s][state]+   smoothed[parola][state]
-                    if temp1 > mVit:
-                        mVit = temp1
+    for tagPreced in trans_prob.keys():
+        if tagPreced != 'FIRST':
+            if parola in parole_sconosciute:
+                if state in parole_sconosciute[parola] and state in trans_prob[tagPreced]:
+                    valoreViterbiCambiato = vit[tagPreced][i] + trans_prob[tagPreced][state]+   parole_sconosciute[parola][state]
+                    if valoreViterbiCambiato > mVit:mVit = valoreViterbiCambiato
                     
-                    temp2 = vit[s][i]+ trans_prob[s][state]
-                    if temp2 > mPath:
-                        mPath = temp2
-                        maxTag = s
+                    pathViterbiCambiato = vit[tagPreced][i]+ trans_prob[tagPreced][state]
+                    if pathViterbiCambiato > mPath:
+                        mPath = pathViterbiCambiato
+                        tag = tagPreced
             else:
-                if (parola in emission_prob and s in trans_prob):
-                    if(state in emission_prob[parola] and state in trans_prob[s]):
-                        temp1 = vit[s][i] + trans_prob[s][state]+   emission_prob[parola][state]
-                        if temp1 > mVit:
-                            mVit = temp1
+                if (parola in emission_prob and tagPreced in trans_prob):
+                    if(state in emission_prob[parola] and state in trans_prob[tagPreced]):
+                        valoreViterbiCambiato = vit[tagPreced][i] + trans_prob[tagPreced][state]+   emission_prob[parola][state]
+                        if valoreViterbiCambiato > mVit:mVit = valoreViterbiCambiato
                         
-                        temp2 = vit[s][i]+ trans_prob[s][state]
-                        if temp2 > mPath:
-                            mPath = temp2
-                            maxTag = s
+                        pathViterbiCambiato = vit[tagPreced][i]+ trans_prob[tagPreced][state]
+                        if pathViterbiCambiato > mPath:
+                            mPath = pathViterbiCambiato
+                            tag = tagPreced
     
-    return mVit, mPath, maxTag
+    return mVit, mPath, tag
 
 
 def decoding(pathTest, tag_della_parola, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag,smoothing1, smoothing2, smoothing3, smoothing4,condizione):
@@ -117,10 +119,10 @@ def decoding(pathTest, tag_della_parola, prob_che_un_tag_precedi_un_altro_tag, p
                     num_totale_parole += 1
                 if line == '\n':
                     res_base = baseline(frase, tag_della_parola )
-                    sm1 = viterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing1)[0]
-                    sm2 = viterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing2)[0]
-                    sm3 = viterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing3)[0]
-                    sm4 = viterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing4)[0]
+                    sm1 = algoritmoDiViterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing1)[0]
+                    sm2 = algoritmoDiViterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing2)[0]
+                    sm3 = algoritmoDiViterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing3)[0]
+                    sm4 = algoritmoDiViterbi(frase, prob_che_un_tag_precedi_un_altro_tag, prob_parola_con_tag, smoothing4)[0]
                     
                     
                     for i, parola in enumerate(frase):
@@ -132,16 +134,11 @@ def decoding(pathTest, tag_della_parola, prob_che_un_tag_precedi_un_altro_tag, p
                             print("smothing4 -->", sm4[parola])
                             print("baseline -->", res_base[parola] )
 
-                        if sm1[parola] == tag_suggerito_dal_file[i]:
-                            paroleCorretteSmoothing1 += 1
-                        if sm2[parola] == tag_suggerito_dal_file[i]:
-                            paroleCorretteSmoothing2 += 1
-                        if sm3[parola] == tag_suggerito_dal_file[i]:
-                            paroleCorretteSmoothing3 += 1
-                        if sm4[parola] == tag_suggerito_dal_file[i]:
-                            paroleCorretteSmoothing4 += 1
-                        if res_base[parola] == tag_suggerito_dal_file[i]:
-                            paroleCorretteBaseline += 1
+                        if sm1[parola] == tag_suggerito_dal_file[i]:paroleCorretteSmoothing1 += 1
+                        if sm2[parola] == tag_suggerito_dal_file[i]:paroleCorretteSmoothing2 += 1
+                        if sm3[parola] == tag_suggerito_dal_file[i]:paroleCorretteSmoothing3 += 1
+                        if sm4[parola] == tag_suggerito_dal_file[i]:paroleCorretteSmoothing4 += 1
+                        if res_base[parola] == tag_suggerito_dal_file[i]:paroleCorretteBaseline += 1
                         
                     tag_suggerito_dal_file = list()
                     frase = list()
@@ -156,4 +153,4 @@ def decoding(pathTest, tag_della_parola, prob_che_un_tag_precedi_un_altro_tag, p
             print("smoothing2: "+str(s2)+" %")
             print("smoothing3: "+str(s3)+" %")
             print("smoothing4: "+str(s4)+" %")
-            print("smooth baseline: "+str(round((paroleCorretteBaseline *100) / num_totale_parole, 2))+"%")
+            print("baseline: "+str(round((paroleCorretteBaseline *100) / num_totale_parole, 2))+"%")
